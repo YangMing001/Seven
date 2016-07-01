6aee3b0c3fa87494ec2ecabccee6ad9f51911f0c
YM <yangming@1yd.me>
2016-07-01 18:45:53 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'  # 官方库
+
+platform :ios,’9.0’
+inhibit_all_warnings!
+
+target :Seven do
+    pod 'AFNetworking'
+end
---
+++ Seven/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seven
+//
+//  Created by YM on 16/7/1.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
